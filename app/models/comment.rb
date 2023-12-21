475a024ae1b9446a018f9ef518c71e2e8d4d8d96
ademola <ademolaowoeye07@gmail.com>
2023-12-21 02:56:47 +0100

    Modal files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :user
   belongs_to :post
-  after_save :update_comment_counter
 
-  def update_comment_counter
-    post.update(comments_counter: post.comments.all.length)
+  after_save :update_post_comment_counter
+
+  def update_post_comment_counter
+    post.increment!(:comments_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,19 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :likes, foreign_key: :post_id
-  has_many :comments, foreign_key: :post_id
+  has_many :likes
+  has_many :comments
+  belongs_to :user
 
-  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-  # validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  # validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  after_save :update_post_user_counter
 
-  after_save :update_post_counter
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def five_recent_comments
-    comments.order(created_at: :desc).limit(5)
+  def update_post_user_counter
+    user.increment!(:posts_counter)
   end
 
-  def update_post_counter
-    author.update(posts_counter: author.posts.all.length)
+  def five_most_recent_comments
+    comments.includes(:user).order(created_at: :desc).first(5)
   end
 end
