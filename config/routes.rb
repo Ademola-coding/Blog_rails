cf285d6b8ec2ca0d4d1567d3f8fdf294bda7cd06
ademola <ademolaowoeye07@gmail.com>
2023-12-22 21:12:53 +0100

    configure files

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:sessions]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
-
-  root "users#index"
-
-  resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show, :new, :create] do 
-      resources :comments, only: [:create, :new] 
-      resources :likes, only: [:create]
-    end
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'users#index'
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create, :destroy]
+    end
+  end
 end
